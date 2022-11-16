88f386ae5d19700cfb91dbad830017583f3a8e63
Gregorio Queiroz <gregorio.queiroz@ipeas.local>
2022-11-16 14:40:18 -0300

    Iniciando o app SERVICOS

--- mecajato/settings.py
+++ mecajato/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mecajato.clientes'
+    'mecajato.clientes',
+    'mecajato.servicos'
 ]
 
 MIDDLEWARE = [
