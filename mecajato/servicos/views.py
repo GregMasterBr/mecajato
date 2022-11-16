25a5b307ec458e86836e1fbab2cba7448c9633ef
Gregorio Queiroz <gregorio.queiroz@ipeas.local>
2022-11-16 14:49:42 -0300

    Criado a rota, a função e o template

--- mecajato/servicos/views.py
+++ mecajato/servicos/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+#from .models import Cliente, Carro
 
-# Create your views here.
+def novo_servico(request):
+    return render(request,'novo_servico.html')
--- mecajato/urls.py
+++ mecajato/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('clientes/', include('mecajato.clientes.urls'))
+    path('clientes/', include('mecajato.clientes.urls')),
+    path('servicos/', include('mecajato.servicos.urls')),
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
