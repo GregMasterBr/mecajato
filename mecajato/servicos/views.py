caaaf7b0a438ac9fd35eac032c9755b3de62baf9
Gregorio Queiroz <gregorio.queiroz@ipeas.local>
2022-11-16 14:52:34 -0300

    Implementação MODEL, VIEW, FORMS and CHOICES

--- mecajato/servicos/views.py
+++ mecajato/servicos/views.py
@@ -2,6 +2,17 @@ from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 #from .models import Cliente, Carro
+from .forms import FormServico
 
 def novo_servico(request):
-    return render(request,'novo_servico.html')
+    if request.method == "GET":
+        form = FormServico()
+        return render(request, 'novo_servico.html', {'form': form})
+    elif request.method == "POST":
+        form = FormServico(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Salvo com sucesso')
+        else:
+            return render(request, 'novo_servico.html', {'form': form})
